fcc4281bf3c85a1acac2164683e7214d1fb2df05
thewebdreamer1 <thewebdreamer@gmail.com>
2026-01-07 18:00:01 -0600

    Refactor AIPV_Posts class methods for improved readability and maintainability; update Spanish translations for API key instructions

--- admin/view.php
+++ admin/view.php
@@ -6,29 +6,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Ensure the user has the appropriate capability to manage options
 if ( !current_user_can( 'manage_options' ) ) {
-    wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'ai-post-visualizer' ) );
+  wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'ai-post-visualizer' ) );
 }
 
 // Setup allowed html
 $allowed_html = array(
-    'div' => array(
-        'class' => array(),
-        'data-history' => array(),
-        'data-post' => array(),
-        'data-type' => array(),
-        'data-alphabetical' => array(),
-        'data-date' => array(),
-        'data-current-page' => array(),
-        'style' => array()
-    ),
-    'img' => array(
-        'src' => array(),
-        'alt' => array(),
-    ),
-    'span' => array(),
-    'strong' => array(),
-    'em' => array(),
-    'a' => array()
+  'div' => array(
+    'class' => array(),
+    'data-history' => array(),
+    'data-post' => array(),
+    'data-type' => array(),
+    'data-alphabetical' => array(),
+    'data-date' => array(),
+    'data-current-page' => array(),
+    'style' => array()
+  ),
+  'img' => array(
+    'src' => array(),
+    'alt' => array(),
+  ),
+  'span' => array(),
+  'strong' => array(),
+  'em' => array(),
+  'a' => array()
 );
 
 // Check if the 'aipv_clear_data' option is set, otherwise set default to false
@@ -57,26 +57,26 @@ $validation = !empty( $dalle_api_key );
 // Begin rendering the admin page view
 ?>
 <div id="aipv-admin-view" class="<?php echo esc_attr( $viewer_mode ); ?>">
+  <?php 
+  // Include the header view for the admin page
+  include_once dirname( __FILE__ ) . '/views/header.php'; 
+  ?>
+  <div class="content-area">
     <?php 
-    // Include the header view for the admin page
-    include_once dirname( __FILE__ ) . '/views/header.php'; 
+    // Include the sidebar for navigation within the plugin
+    include_once dirname( __FILE__ ) . '/views/sidebar.php'; 
     ?>
-    <div class="content-area">
-        <?php 
-        // Include the sidebar for navigation within the plugin
-        include_once dirname( __FILE__ ) . '/views/sidebar.php'; 
-        ?>
-        <div class="main-content">
-            <?php 
-            // Include the posts view for managing posts within the plugin
-            include_once dirname( __FILE__ ) . '/views/posts.php'; 
-            
-            // Include the generate view for generating new images
-            include_once dirname( __FILE__ ) . '/views/generate.php'; 
-            
-            // Include the settings view for managing plugin settings
-            include_once dirname( __FILE__ ) . '/views/settings.php'; 
-            ?>
-        </div>
+    <div class="main-content">
+      <?php 
+      // Include the posts view for managing posts within the plugin
+      include_once dirname( __FILE__ ) . '/views/posts.php'; 
+      
+      // Include the generate view for generating new images
+      include_once dirname( __FILE__ ) . '/views/generate.php'; 
+      
+      // Include the settings view for managing plugin settings
+      include_once dirname( __FILE__ ) . '/views/settings.php'; 
+      ?>
     </div>
+  </div>
 </div>
--- classes/aipv-ai-processor.php
+++ classes/aipv-ai-processor.php
@@ -10,12 +10,12 @@ class AIPV_AI_Processor {
      * Constructor to initialize AJAX actions if in the admin.
      */
     public function __construct() {
-        if ( is_admin() ) {
-            add_action( 'wp_ajax_aipv_get_dalle_images', array( $this, 'aipv_get_dalle_images' ) );
-            add_action( 'wp_ajax_aipv_set_dalle_image', array( $this, 'aipv_set_dalle_image' ) );
-            add_action( 'wp_ajax_aipv_revert_featured_image', array( $this, 'aipv_revert_featured_image' ) );
-            add_action( 'wp_ajax_aipv_load_dalle_history', array( $this, 'aipv_load_dalle_history' ) );
-        }
+      if ( is_admin() ) {
+        add_action( 'wp_ajax_aipv_get_dalle_images', array( $this, 'aipv_get_dalle_images' ) );
+        add_action( 'wp_ajax_aipv_set_dalle_image', array( $this, 'aipv_set_dalle_image' ) );
+        add_action( 'wp_ajax_aipv_revert_featured_image', array( $this, 'aipv_revert_featured_image' ) );
+        add_action( 'wp_ajax_aipv_load_dalle_history', array( $this, 'aipv_load_dalle_history' ) );
+      }
     }
 
     /**
@@ -25,77 +25,78 @@ class AIPV_AI_Processor {
      */
     public function aipv_get_dalle_images() {
 
-        // Nonce validation
-		check_ajax_referer( 'aipv_nonce_action', 'aipv_nonce' );
+      // Nonce validation
+      check_ajax_referer( 'aipv_nonce_action', 'aipv_nonce' );
 
-        // Sanitize input
-        $post_id = isset( $_GET['post_id'] ) ? intval( wp_unslash( $_GET['post_id'] ) ) : '';
-        $prompt = isset( $_GET['prompt'] ) ? sanitize_text_field( wp_unslash( $_GET['prompt'] ) ) : '';
-        $n = isset( $_GET['n'] ) ? intval( wp_unslash( $_GET['n'] ) ) : 1;
-        $size = isset( $_GET['size'] ) ? sanitize_text_field( wp_unslash( $_GET['size'] ) ) : '256x256';
+      // Sanitize input
+      $post_id = isset( $_GET['post_id'] ) ? intval( wp_unslash( $_GET['post_id'] ) ) : '';
+      $prompt = isset( $_GET['prompt'] ) ? sanitize_text_field( wp_unslash( $_GET['prompt'] ) ) : '';
+      $n = isset( $_GET['n'] ) ? intval( wp_unslash( $_GET['n'] ) ) : 1;
+      $size = isset( $_GET['size'] ) ? sanitize_text_field( wp_unslash( $_GET['size'] ) ) : '256x256';
 
-        // Sanitize prompt for use as image title
-        $image_title = implode( '-', array_slice( explode( ' ', $prompt ), 0, 6 ) );
+      // Sanitize prompt for use as image title
+      $image_title = implode( '-', array_slice( explode( ' ', $prompt ), 0, 6 ) );
 
-        // Send the API request
-        $api_data = $this->aipv_api_request( $prompt, $n, $size );
+      // Send the API request
+      $api_data = $this->aipv_api_request( $prompt, $n, $size );
 
-		// Check if api data valid
-        if( $api_data && !isset( $api_data->status ) ) {
+      // Check if api data valid
+      if( $api_data && !isset( $api_data->status ) ) {
 
-			// Get urls and set empty content and generated_images variables
-            $urls = $api_data['data'];
-            $content = '';
-            $generated_images = array();
+        // Get urls and set empty content and generated_images variables
+        $urls = $api_data['data'];
+        $content = '';
+        $generated_images = array();
 
-            // Loop through the API response to generate images
-            foreach ( $urls as $i => $url ) {
+        // Loop through the API response to generate images
+        foreach ( $urls as $i => $url ) {
 
-				// Get iamge url and update generated_images array
-                $image_id = $this->aipv_upload_images_to_library( $url['url'], $image_title . '-' . $i );
-                $image_url = wp_get_attachment_url( $image_id );
-                $generated_images[] = $image_id;
+          // Get image url and update generated_images array
+          $image_id = $this->aipv_upload_images_to_library( $url['url'], $image_title . '-' . $i );
+          $image_url = wp_get_attachment_url( $image_id );
+          $generated_images[] = $image_id;
 
-                // Build the HTML content for the images
-                $content .= '<div class="post-card" data-image="' . esc_attr( $image_id ) . '">';
-                $content .= '<div class="image" style="background-image: url(' . esc_url( $image_url ) . ')"></div>';
-                $content .= '<div class="set-image">';
-                $content .= '<div class="plus">';
-                $content .= '<img src="' . esc_url( AIPV_PLUGIN_DIR . 'admin/views/img/plus.svg' ) . '" />';
-                $content .= '</div>';
-                $content .= '<div class="set-text">' . __( 'Set Featured Image', 'ai-post-visualizer' ) . '</div>';
-                $content .= '<div class="current-text">' . __( 'Current Featured Image', 'ai-post-visualizer' ) . '</div>';
-                $content .= '</div></div>';
-            }
+          // Build the HTML content for the images
+          $content .= '<div class="post-card" data-image="' . esc_attr( $image_id ) . '">';
+          $content .= '<div class="image" style="background-image: url(' . esc_url( $image_url ) . ')"></div>';
+          $content .= '<div class="set-image">';
+          $content .= '<div class="plus">';
+          $content .= '<img src="' . esc_url( AIPV_PLUGIN_DIR . 'admin/views/img/plus.svg' ) . '" />';
+          $content .= '</div>';
+          $content .= '<div class="set-text">' . __( 'Set Featured Image', 'ai-post-visualizer' ) . '</div>';
+          $content .= '<div class="current-text">' . __( 'Current Featured Image', 'ai-post-visualizer' ) . '</div>';
+          $content .= '</div></div>';
 
-            // Insert post history into the 'aipv_history' custom post type
-            if ( !empty( $content ) ) {
-                $history = wp_insert_post( [
-                    'post_type'   => 'aipv_history',
-                    'post_status' => 'publish',
-                    'post_title'  => $prompt,
-                    'post_name'   => uniqid( 'aipv_' ),
-                ] );
+        }
 
-                // Store meta data for the history
-                update_post_meta( $history, 'prompt', $prompt );
-                update_post_meta( $history, 'images', $generated_images );
-                update_post_meta( $history, 'resolution', $size );
+        // Insert post history into the 'aipv_history' custom post type
+        if ( !empty( $content ) ) {
+          $history = wp_insert_post( [
+            'post_type'   => 'aipv_history',
+            'post_status' => 'publish',
+            'post_title'  => $prompt,
+            'post_name'   => uniqid( 'aipv_' ),
+          ] );
 
-				// Send json response
-                wp_send_json( $content );
+          // Store meta data for the history
+          update_post_meta( $history, 'prompt', $prompt );
+          update_post_meta( $history, 'images', $generated_images );
+          update_post_meta( $history, 'resolution', $size );
 
-            } else {
-
-				// Send json error
-                wp_send_json_error( 'Error with prompt.' );
-
-            }
+          // Send json response
+          wp_send_json( $content );
 
         } else {
-            $content = '<div class="invalid-api-key">' . __( 'Please go to the Settings tab and sign up for a plan before continuing.', 'ai-post-visualizer' ) . '</div>';
-            wp_send_json( $content );
+
+          // Send json error
+          wp_send_json_error( 'Error with prompt.' );
+
         }
+
+      } else {
+        $content = '<div class="invalid-api-key">' . __( 'Please go to the Settings tab and add your API key before continuing.', 'ai-post-visualizer' ) . '</div>';
+        wp_send_json( $content );
+      }
 
     }
 
@@ -107,7 +108,7 @@ class AIPV_AI_Processor {
     public function aipv_set_dalle_image() {
 
         // Nonce validation
-		check_ajax_referer( 'aipv_nonce_action', 'aipv_nonce' );
+		    check_ajax_referer( 'aipv_nonce_action', 'aipv_nonce' );
 
         // Sanitize input
         $post_id = isset( $_GET['post_id'] ) ? intval( wp_unslash( $_GET['post_id'] ) ) : '';
@@ -116,14 +117,14 @@ class AIPV_AI_Processor {
         // Backup original featured image if not already done
         $original = get_post_thumbnail_id( $post_id );
         if ( !get_post_meta( $post_id, 'aipv_revert', true ) ) {
-            update_post_meta( $post_id, 'aipv_revert', $original );
+          update_post_meta( $post_id, 'aipv_revert', $original );
         }
 
         // Set the new featured image
         set_post_thumbnail( $post_id, $image_id );
         $image_url = wp_get_attachment_url( $image_id );
 
-		// Send json response
+		    // Send json response
         wp_send_json( $image_url );
 
     }
@@ -136,7 +137,7 @@ class AIPV_AI_Processor {
     public function aipv_revert_featured_image() {
 
         // Nonce validation
-		check_ajax_referer( 'aipv_nonce_action', 'aipv_nonce' );
+		    check_ajax_referer( 'aipv_nonce_action', 'aipv_nonce' );
 
         // Sanitize input
         $post_id = isset( $_GET['post_id'] ) ? intval( wp_unslash( $_GET['post_id'] ) ) : '';
@@ -146,10 +147,10 @@ class AIPV_AI_Processor {
         set_post_thumbnail( $post_id, $original_img );
         delete_post_meta( $post_id, 'aipv_revert' );
 
-		// Get image attachment url
+		    // Get image attachment url
         $image_url = wp_get_attachment_url( $original_img );
 
-		// Send json response
+		    // Send json response
         wp_send_json( $image_url );
 
     }
@@ -162,36 +163,36 @@ class AIPV_AI_Processor {
     public function aipv_load_dalle_history() {
 
         // Nonce validation
-		check_ajax_referer( 'aipv_nonce_action', 'aipv_nonce' );
+		    check_ajax_referer( 'aipv_nonce_action', 'aipv_nonce' );
 
         // Sanitize input
         $post_id = isset( $_GET['post_id'] ) ? intval( $_GET['post_id'] ) : '';
         $images = get_post_meta( $post_id, 'images', true );
 
-		// Set empty content variable
+	    	// Set empty content variable
         $content = '';
 
         // Loop through and generate HTML for each stored image
         foreach( $images as $img ) {
 
-			// Get image attachment url
-            $image_url = wp_get_attachment_url( $img );
+          // Get image attachment url
+          $image_url = wp_get_attachment_url( $img );
 
-			// Check if image url available and update content
-            if ( $image_url ) {
-                $content .= '<div class="post-card" data-image="' . esc_attr( $img ) . '">';
-                $content .= '<div class="image" style="background-image: url(' . esc_url( $image_url ) . ')"></div>';
-                $content .= '<div class="set-image">';
-                $content .= '<div class="plus">';
-                $content .= '<img src="' . esc_url( AIPV_PLUGIN_DIR . 'admin/views/img/plus.svg' ) . '" />';
-                $content .= '</div>';
-                $content .= '<div class="set-text">' . __( 'Set Featured Image', 'ai-post-visualizer' ) . '</div>';
-                $content .= '<div class="current-text">' . __( 'Current Featured Image', 'ai-post-visualizer' ) . '</div>';
-                $content .= '</div></div>';
-            }
+          // Check if image url available and update content
+          if ( $image_url ) {
+            $content .= '<div class="post-card" data-image="' . esc_attr( $img ) . '">';
+            $content .= '<div class="image" style="background-image: url(' . esc_url( $image_url ) . ')"></div>';
+            $content .= '<div class="set-image">';
+            $content .= '<div class="plus">';
+            $content .= '<img src="' . esc_url( AIPV_PLUGIN_DIR . 'admin/views/img/plus.svg' ) . '" />';
+            $content .= '</div>';
+            $content .= '<div class="set-text">' . __( 'Set Featured Image', 'ai-post-visualizer' ) . '</div>';
+            $content .= '<div class="current-text">' . __( 'Current Featured Image', 'ai-post-visualizer' ) . '</div>';
+            $content .= '</div></div>';
+          }
         }
 
-		// Send json response
+		    // Send json response
         wp_send_json( $content );
 
     }
@@ -211,40 +212,40 @@ class AIPV_AI_Processor {
     
         // Ensure the API key exists
         if ( !$dalle_api_key ) {
-            return false;
+          return false;
         }
     
         // API request headers
         $headers = [
-            'Authorization' => 'Bearer ' . $dalle_api_key,
-            'Content-Type'  => 'application/json',
+          'Authorization' => 'Bearer ' . $dalle_api_key,
+          'Content-Type'  => 'application/json',
         ];
     
         // Prepare the request data using wp_json_encode()
         $body = wp_json_encode([
-            'prompt' => $prompt,
-            'n'      => $n,
-            'size'   => $size,
+          'prompt' => $prompt,
+          'n'      => $n,
+          'size'   => $size,
         ]);
     
         // Perform the request using wp_remote_post
         $response = wp_remote_post( 'https://api.openai.com/v1/images/generations', [
-            'headers' => $headers,
-            'body'    => $body,
-            'timeout' => 45, // Set an appropriate timeout
+          'headers' => $headers,
+          'body'    => $body,
+          'timeout' => 45, // Set an appropriate timeout
         ]);
     
         // Check for errors
         if ( is_wp_error( $response ) ) {
-            // error_log( 'HTTP request failed: ' . $response->get_error_message() );
-            return false;
+          // error_log( 'HTTP request failed: ' . $response->get_error_message() );
+          return false;
         }
     
         // Check if the response code is 200 OK
         $http_status = wp_remote_retrieve_response_code( $response );
         if ( $http_status !== 200 ) {
-            // error_log( 'HTTP error: ' . $http_status . ' Response: ' . wp_remote_retrieve_body( $response ) );
-            return false;
+          // error_log( 'HTTP error: ' . $http_status . ' Response: ' . wp_remote_retrieve_body( $response ) );
+          return false;
         }
     
         // Decode and return the response
@@ -262,71 +263,71 @@ class AIPV_AI_Processor {
     public function aipv_upload_images_to_library ( $url, $title = null ) {
 
         // Setup required paths for image upload
-		require_once( ABSPATH . '/wp-load.php' );
-		require_once( ABSPATH . '/wp-admin/includes/image.php' );
-		require_once( ABSPATH . '/wp-admin/includes/file.php' );
-		require_once( ABSPATH . '/wp-admin/includes/media.php' );
+        require_once( ABSPATH . '/wp-load.php' );
+        require_once( ABSPATH . '/wp-admin/includes/image.php' );
+        require_once( ABSPATH . '/wp-admin/includes/file.php' );
+        require_once( ABSPATH . '/wp-admin/includes/media.php' );
 
-		// Download url to a temp file
-		$tmp = download_url( $url );
-		if( is_wp_error( $tmp ) ) {
-			return false;
-		}
+        // Download url to a temp file
+        $tmp = download_url( $url );
+        if( is_wp_error( $tmp ) ) {
+          return false;
+        }
 
-		// Get the filename and extension ("photo.png" => "photo", "png")
-		$filename = pathinfo( $url, PATHINFO_FILENAME );
-		$extension = pathinfo( $url, PATHINFO_EXTENSION );
+        // Get the filename and extension ("photo.png" => "photo", "png")
+        $filename = pathinfo( $url, PATHINFO_FILENAME );
+        $extension = pathinfo( $url, PATHINFO_EXTENSION );
 
-		// An extension is required or else WordPress will reject the upload
-		if( ! $extension ) {
-			// Look up mime type, example: "/photo.png" -> "image/png"
-			$mime = mime_content_type( $tmp );
-			$mime = is_string( $mime ) ? sanitize_mime_type( $mime ) : false;
+        // An extension is required or else WordPress will reject the upload
+        if( ! $extension ) {
+          // Look up mime type, example: "/photo.png" -> "image/png"
+          $mime = mime_content_type( $tmp );
+          $mime = is_string( $mime ) ? sanitize_mime_type( $mime ) : false;
 
-			// Only allow certain mime types because mime types do not always end in a valid extension (see the .doc example below)
-			$mime_extensions = array(
-				// mime_type         => extension (no period)
-				'text/plain'         => 'txt',
-				'text/csv'           => 'csv',
-				'application/msword' => 'doc',
-				'image/jpg'          => 'jpg',
-				'image/jpeg'         => 'jpeg',
-				'image/gif'          => 'gif',
-				'image/png'          => 'png',
-				'video/mp4'          => 'mp4',
-			);
+          // Only allow certain mime types because mime types do not always end in a valid extension (see the .doc example below)
+          $mime_extensions = array(
+            // mime_type         => extension (no period)
+            'text/plain'         => 'txt',
+            'text/csv'           => 'csv',
+            'application/msword' => 'doc',
+            'image/jpg'          => 'jpg',
+            'image/jpeg'         => 'jpeg',
+            'image/gif'          => 'gif',
+            'image/png'          => 'png',
+            'video/mp4'          => 'mp4',
+          );
 
-			if ( isset( $mime_extensions[$mime] ) ) {
-				// Use the mapped extension
-				$extension = $mime_extensions[$mime];
-			} else{
-				// Could not identify extension
-				wp_delete_file( $tmp );
-				return false;
-			}
-		}
+          if ( isset( $mime_extensions[$mime] ) ) {
+            // Use the mapped extension
+            $extension = $mime_extensions[$mime];
+          } else{
+            // Could not identify extension
+            wp_delete_file( $tmp );
+            return false;
+          }
+        }
 
-		// Upload by "sideloading": "the same way as an uploaded file is handled by media_handle_upload"
-		$filename = md5( uniqid( md5( $filename ), true ) ) . '_' . time();
-		$args = array(
-			'name' => "$filename.$extension",
-			'tmp_name' => $tmp,
-		);
+        // Upload by "sideloading": "the same way as an uploaded file is handled by media_handle_upload"
+        $filename = md5( uniqid( md5( $filename ), true ) ) . '_' . time();
+        $args = array(
+          'name' => "$filename.$extension",
+          'tmp_name' => $tmp,
+        );
 
-		// Do the upload
-		$attachment_id = media_handle_sideload( $args, 0, $title );
+        // Do the upload
+        $attachment_id = media_handle_sideload( $args, 0, $title );
 
-		// Cleanup temp file
-		wp_delete_file( $tmp );
+        // Cleanup temp file
+        wp_delete_file( $tmp );
 
-		// Error uploading
-		if ( is_wp_error( $attachment_id ) ) {
-			return false;
-		}
+        // Error uploading
+        if ( is_wp_error( $attachment_id ) ) {
+          return false;
+        }
 
-		// Success, return attachment ID (int)
-		return (int) $attachment_id;
+      // Success, return attachment ID (int)
+      return (int) $attachment_id;
 
-	}
+	  }
 
 }
